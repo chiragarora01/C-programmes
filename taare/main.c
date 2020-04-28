40cbfd956d811b10c3dbe8e12ca2eaf3cbd23f2d
chirag <arorachirag155@gmail.com>
2020-04-28 22:33:14 +0530

    Add files via upload

---
+++ taare/main.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+int main()
+/*                                //printing name using while
+  {char string[]="hole";
+       int i=0;
+       while(i<=4)
+                     {printf("%c",string[i]);
+                           i++; }
+  }
+                                //printing name using while
+{
+    char string[]="hole";
+    int i=0;
+    while (string[i]!='\0')
+        {printf("%c",string[i]);
+        i++;
+        }
+}
+
+
+ // strcpy                             //copying it in new string
+{
+    char st1[100],st2[100],st3[100];
+    puts("enter your first string");
+    gets(st1);
+
+    puts("enter your second  string");
+    gets(st2);
+
+    strcpy(st3,"i will succed it ");
+    printf("you entered value of string one a as \t\t %s \n",st1);
+    printf("you entered value of string one a as \t\t %s \n",st2);
+    printf("you entered value of string one a as \t\t %s",st3);
+}
+
+
+                                //compare two string (strcmp)***************
+{
+    char st1[500]="jerry",st2[500]="ferry",st3[500];
+
+    int i=strcmp(st1,"jerry");
+    int j=strcmp(st1,st2);
+    int k= strcmp(st1,"jerry boy ");
+    printf("%d\t%d\t%d",i,j,k);
+
+    // if string are same it will give the same value but if strings are difference between aissec value of first comapred no
+
+}
+
+                //strcat                //concantent two stringstream
+{
+    char st1[500],st2[500],st3[500];
+    gets(st1);
+    gets(st2);
+    strcat(st1,"with me ");
+    printf("%s\n",st1);
+printf("%s",st2);
+}
+
+
+                                    //converting a string to lower and upper case
+{
+    char st1[500],st2[500],st3[500];
+    gets(st1);
+    gets(st2);
+    strupr(st1);
+    strlwr(st2);
+    printf("%s %s",st1,st2);
+}
+*/
+                                    //
+
+
